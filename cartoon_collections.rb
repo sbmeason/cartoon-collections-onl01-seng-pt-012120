bea980704a108a233bbc84e0f79479aa2fd47587
Samantha Meason <sbmeason@gmail.com>
2020-02-01 03:39:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
 end
