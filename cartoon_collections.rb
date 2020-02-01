f043440ed23da8abf68967dca40b7aa1897152bd
Samantha Meason <sbmeason@gmail.com>
2020-02-01 03:22:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(dwarves)
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |planeteer|
+    "#{planeteer_calls.upcase}"
+  
 end
 
 def long_planeteer_calls# code an argument here
