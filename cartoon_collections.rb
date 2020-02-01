c1479aca8c3bbce42a1d8b22326ee1b78e871223
Samantha Meason <sbmeason@gmail.com>
2020-02-01 03:24:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do {|planeteer| planeteer_calls.capitalize}
-    
-  
 end
+  
 
 def long_planeteer_calls# code an argument here
   # Your code here
